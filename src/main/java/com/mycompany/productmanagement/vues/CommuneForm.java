51945e3349a449ad54408b60eb8a0f8f738fb7b6
Mathdola <matthiasdola@gmail.com>
2019-09-25 12:28:19 +0000

    Change class description in their header

--- src/main/java/com/mycompany/productmanagement/vues/CommuneForm.java
+++ src/main/java/com/mycompany/productmanagement/vues/CommuneForm.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.productmanagement.vues;
 
 import com.mycompany.productmanagement.controllers.CommuneController;
@@ -12,7 +7,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author macbookair
+ * @author Matthias DOLA
  */
 public class CommuneForm extends javax.swing.JFrame {
 
